4399f9dce839c9f2a5ad5ae294c3406ab6bc3c39
juanca <juanovandob@gmail.com>
2022-06-29 06:55:57 +0000

    crear borrador antes d publicar y boton d publicar

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-]
+    path('drafts/', views.post_draft_list, name='post_draft_list'),  #post - borradores antes de publicar
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'), #Donde daremos la orden de publicar
+ ]                             
 
